32241baa64e19993e20fcc97b7b193fd9b5160a1
Cocoteax <97997374+Cocoteax@users.noreply.github.com>
2023-05-29 13:13:25 +0800

    add navbar and image assets

---
+++ src/components/NavBar.js
@@ -0,0 +1,97 @@
+import { Navbar, Container, Nav, NavDropdown } from "react-bootstrap";
+import { useState, useEffect } from "react";
+import logo from "../assets/img/logo.svg"
+import navIcon1 from "../assets/img/nav-icon1.svg"
+import navIcon2 from "../assets/img/nav-icon2.svg"
+import navIcon3 from "../assets/img/nav-icon3.svg"
+
+function NavBar() {
+  const [activeLink, setActiveLink] = useState("about");
+  const [scrolled, setScrolled] = useState("false");
+
+  // handle scrolling behaviour => without scrolling, navbar is transparent. with scrolling, navbar turns black
+  useEffect(() => {
+    const onScroll = () => {
+      // If user scrolls down by 50
+      if (window.scrollY > 50) {
+        setScrolled(true);
+      } else {
+        setScrolled(false);
+      }
+    };
+
+    // Add scroll event listener when component mounts
+    window.addEventListener("scroll", onScroll);
+
+    // Since we added event listener via useEffect, we need a clean up function to remove it each time the component unmounts
+    return () => window.removeEventListener("scroll", onScroll);
+  }, []);
+
+  const updateActiveLinkHandler = (link) => {
+    setActiveLink(link);
+  };
+
+  return (
+    <Navbar expand="lg" className={scrolled ? "scrolled" : ""}>
+      <Container>
+        <Navbar.Brand href="#about">
+          <img src={logo} alt="Logo" />
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link
+              href="#about"
+              className={
+                activeLink === "about" ? "active navbar-link" : "navbar-link"
+              }
+              onClick={() => updateActiveLinkHandler("about")}
+            >
+              About
+            </Nav.Link>
+            <Nav.Link
+              href="#skills"
+              className={
+                activeLink === "skills" ? "active navbar-link" : "navbar-link"
+              }
+              onClick={() => updateActiveLinkHandler("skills")}
+            >
+              Skills
+            </Nav.Link>
+            <Nav.Link
+              href="#experience"
+              className={
+                activeLink === "experience"
+                  ? "active navbar-link"
+                  : "navbar-link"
+              }
+              onClick={() => updateActiveLinkHandler("experience")}
+            >
+              Experience
+            </Nav.Link>
+          </Nav>
+          {/* For social icons */}
+          <span className="navbar-text">
+            <div className="social-icon">
+              {/* Add social links on href */}
+              <a href="#">
+                <img src={navIcon1} alt="" />
+              </a>
+              <a href="#">
+                <img src={navIcon2} alt="" />
+              </a>
+              <a href="#">
+                <img src={navIcon3} alt="" />
+              </a>
+            </div>
+            <button className="vvd" onClick={() => console.log("connect")}>
+              <span>Let's Connect</span>
+            </button>
+          </span>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+}
+
+export default NavBar;
